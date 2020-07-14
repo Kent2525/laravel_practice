afae9f1b68caa3807b9d061fac6988be5c921e07
Shimizu Kento <aibo396@yahoo.co.jp>
2020-07-14 13:30:32 +0000

    11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1"> 
+@extends('layouts.profile')
 
-    <title>My News</title>
-  </head>
-  <body>
-    <h1>Profile作成画面</h1>
-  </body>
-</html>
+@section('title', 'My Profile')
+
+@section('content')
+<!DOCTYPE html>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection

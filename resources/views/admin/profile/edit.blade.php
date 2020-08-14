74b190bc679deaaebdd9265736bb5823aa617ed4
Shimizu Kento <aibo396@yahoo.co.jp>
2020-08-14 08:24:03 +0000

    17

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -51,19 +51,19 @@
             </div>
           </div>
         </form>
-        <!--課題の下書き-->
-        <!--<div class="row mt-5">-->
-        <!--  <div class="col-md-4 mx-auto">-->
-        <!--    <h2>編集履歴</h2>-->
-        <!--    <ul class="list-group">-->
-        <!--      @if ($news_form->histories !=NULL)-->
-        <!--        @foreach ($news_form->histories as $history)-->
-        <!--          <li class="list-group-item">{{ $history->edited_at }}</li>-->
-        <!--        @endforeach-->
-        <!--      @endif-->
-        <!--    </ul>-->
-        <!--  </div>-->
-        <!--</div>-->
+        <!--課題17の追記-->
+        <div class="row mt-5">
+          <div class="col-md-4 mx-auto">
+            <h2>編集履歴</h2>
+            <ul class="list-group">
+              @if ($profile_form->profile_histories !=NULL)
+                @foreach ($profile_form->profile_histories as $history)
+                  <li class="list-group-item">{{ $history->edited_at }}</li>
+                @endforeach
+              @endif
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
